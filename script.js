fb4a00dcd024c32cf09a3b10a91056afc7650f2c
ranimishra2022 <108725598+ranimishra2022@users.noreply.github.com>
2022-08-22 11:58:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const tipcalcy = () => {
+    let amount = document.getElementById('bill_amount').value;
+    let perc = document.getElementById('tip_perc').value;
+
+    let tip = amount * (perc/100);
+    let total = parseInt(amount) + parseInt(tip); /*parseInt() or Number() both can use */
+
+
+    document.getElementById('tip_total').value = tip;
+    document.getElementById('total_billed').value = total;
+}
